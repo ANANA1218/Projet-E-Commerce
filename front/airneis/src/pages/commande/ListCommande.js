8550852fce25716e92adfe63ff6128087de4cb98
Annabelle <annabelle.bosse.pro@gmail.com>
2023-04-28 11:23:59 +0200

    Liste des commande user Friendly

--- front/airneis/src/pages/commande/ListCommande.js
+++ front/airneis/src/pages/commande/ListCommande.js
@@ -1,87 +1,113 @@
 import React from "react";
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Divider,
+} from "@mui/material";
 
-const orders = [
-  {
-    id: 1,
-    date: "2022-04-26",
-    customerName: "Jane Smith",
-    items: [
-      { name: "Product 1", quantity: 2, price: 10 },
-      { name: "Product 2", quantity: 1, price: 20 },
-      { name: "Product 3", quantity: 3, price: 15 }
-    ]
-  },
-  {
-    id: 2,
-    date: "2022-07-25",
-    customerName: "Jane Smith",
-    items: [
-      { name: "Product 4", quantity: 1, price: 25 },
-      { name: "Product 5", quantity: 2, price: 12 }
-    ]
-  },
-  {
-    id: 3,
-    date: "2022-08-24",
-    customerName: "Jane Smith",
-    items: [
-      { name: "Product 6", quantity: 3, price: 18 },
-      { name: "Product 7", quantity: 1, price: 30 },
-      { name: "Product 8", quantity: 2, price: 22 }
-    ]
-  }
-];
+const OrderHistoryPage = () => {
+  // Exemple de fausses données de commandes
+  const orders = [
+    {
+      id: 1,
+      date: "2023-04-28",
+      items: [
+        {
+          id: 1,
+          name: "Produit 1",
+          image: "https://example.com/product1.jpg",
+          quantity: 2,
+          price: 10,
+        },
+        {
+          id: 2,
+          name: "Produit 2",
+          image: "https://example.com/product2.jpg",
+          quantity: 1,
+          price: 20,
+        },
+      ],
+      totalPrice: 40,
+    },
+    {
+      id: 2,
+      date: "2023-04-27",
+      items: [
+        {
+          id: 3,
+          name: "Produit 3",
+          image: "https://example.com/product3.jpg",
+          quantity: 3,
+          price: 15,
+        },
+      ],
+      totalPrice: 45,
+    },
+  ];
 
-const OrdersPage = () => {
   return (
-    <Box sx={{ p: 2 }}>
+    <Container maxWidth="sm">
       <br></br>
-      <br></br>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="orders table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell align="right">Date</TableCell>
-              <TableCell align="right">Customer Name</TableCell>
-              <TableCell align="right">Items</TableCell>
-              <TableCell align="right">Total</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
+      <Box mt={4}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Historique des commandes
+        </Typography>
+      </Box>
+      <Box mt={2}>
+        {orders.length === 0 ? (
+          <Typography variant="body1" align="center">
+            Aucune commande trouvée.
+          </Typography>
+        ) : (
+          <List>
             {orders.map((order) => (
-              <TableRow
-                key={order.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {order.id}
-                </TableCell>
-                <TableCell align="right">{order.date}</TableCell>
-                <TableCell align="right">{order.customerName}</TableCell>
-                <TableCell align="right">
-                  <ul style={{ listStyleType: "none", padding: 0 }}>
+              <Paper key={order.id} elevation={2} sx={{ mb: 2 }}>
+                <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                    <Avatar alt="Order" src="https://example.com/order.png" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={`Commande #${order.id}`}
+                    secondary={`Date: ${order.date}`}
+                  />
+                </ListItem>
+                <Divider />
+                <Box p={2}>
+                  <List disablePadding>
                     {order.items.map((item) => (
-                      <li key={item.name}>
-                        {item.quantity} x {item.name}
-                      </li>
+                      <ListItem key={item.id} disableGutters>
+                        <ListItemAvatar>
+                          <Avatar alt={item.name} src={item.image} />
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={item.name}
+                          secondary={`Quantité: ${item.quantity}`}
+                        />
+                        <Typography variant="body1">{`Prix: $${item.price}`}</Typography>
+                      </ListItem>
                     ))}
-                  </ul>
-                </TableCell>
-                <TableCell align="right">
-                  {order.items.reduce(
-                    (total, item) => total + item.quantity * item.price,
-                    0 
-                  )} €
-                </TableCell>
-              </TableRow>
+                  </List>
+                  <Divider />
+                  <Box display="flex" justifyContent="flex-end" alignItems="center" mt={2}>
+                    <Typography variant="body1" fontWeight="bold">
+                      Total: ${order.totalPrice}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Paper>
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+          </List>
+        )}
+      </Box>
+    </Container>
   );
 };
 
-export default OrdersPage;
+export default OrderHistoryPage;
