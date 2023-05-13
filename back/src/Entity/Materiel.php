c2dde142f38a67c229358559a6adec67566ff8c8
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-05-13 21:32:08 +0200

    Revert "update Materiel"
    This reverts commit 6e7b736cf8ab1291cc7467beba24124f0737f6e1.

--- back/src/Entity/Materiel.php
+++ back/src/Entity/Materiel.php
@@ -11,9 +11,12 @@ class Materiel
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
     private ?int $id_materiel = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $nom = null;
 
     public function getIdMateriel(): ?int
@@ -21,13 +24,22 @@ class Materiel
         return $this->id_materiel;
     }
 
+    public function setIdMateriel(int $id_materiel): self
+    {
+        $this->id_materiel = $id_materiel;
+
+        return $this;
+    }
+
     public function getNom(): ?string
     {
+        return $this->nom;
         return $this->nom;
     }
 
     public function setNom(string $nom): self
     {
+        $this->nom = $nom;
         $this->nom = $nom;
 
         return $this;
