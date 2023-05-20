d6eb68994f6c6ea8db6acf1cad84c58ac4dedb74
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-05-20 12:18:12 +0200

    change custom enum type

--- back/migrations/Version20230514221601.php
+++ back/migrations/Version20230514221601.php
@@ -20,7 +20,7 @@ final class Version20230514221601 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE commande (id_commande INT AUTO_INCREMENT NOT NULL, id_reduction INT NOT NULL, id_utilisateur INT NOT NULL, date_commande DATE NOT NULL, statut ENUM(\'en attente de confirmation\',\'en cours\',\'expedie\',\'livre\',\'annule\',\'retour\') COMMENT \'(DC2Type:statut_enum)\' NOT NULL, prix_total DOUBLE PRECISION NOT NULL, INDEX IDX_6EEAA67D31D2E84F (id_reduction), INDEX IDX_6EEAA67D50EAE44 (id_utilisateur), PRIMARY KEY(id_commande)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE commande (id_commande INT AUTO_INCREMENT NOT NULL, id_reduction INT NULL, id_utilisateur INT NOT NULL, date_commande DATE NOT NULL, statut VARCHAR(255) NOT NULL, prix_total DOUBLE PRECISION NOT NULL, INDEX IDX_6EEAA67D31D2E84F (id_reduction), INDEX IDX_6EEAA67D50EAE44 (id_utilisateur), PRIMARY KEY(id_commande)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE commande_adresse (id_commande INT NOT NULL, id_adresse INT NOT NULL, INDEX IDX_35E47AB63E314AE8 (id_commande), INDEX IDX_35E47AB61DC2A166 (id_adresse), PRIMARY KEY(id_commande, id_adresse)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67D31D2E84F FOREIGN KEY (id_reduction) REFERENCES reduction (id_reduction)');
         $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67D50EAE44 FOREIGN KEY (id_utilisateur) REFERENCES utilisateur (id_utilisateur)');
--- back/src/Entity/Commande.php
+++ back/src/Entity/Commande.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CommandeRepository::class)]
 class Commande
@@ -19,14 +20,15 @@ class Commande
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $date_commande = null;
 
-    #[ORM\Column(type: 'statut_enum')]
+    #[ORM\Column(type: 'string')]
+    #[Assert\Choice(choices: ['en attente de confirmation', 'en cours', 'expedie', 'livre', 'annule', 'retour'])]
     private $statut = null;
 
     #[ORM\Column]
     private ?float $prix_total = null;
 
     #[ORM\ManyToOne(inversedBy: 'commandes')]
-    #[ORM\JoinColumn(name: 'id_reduction', referencedColumnName: 'id_reduction', nullable: false)]
+    #[ORM\JoinColumn(name: 'id_reduction', referencedColumnName: 'id_reduction', nullable: true)]
     private ?Reduction $id_reduction = null;
 
     #[ORM\ManyToOne(inversedBy: 'commandes')]
