88636bfb1d6dc3cab75038379febdcef5f22d126
Annabelle <annabelle.bosse.pro@gmail.com>
2023-06-10 18:41:59 +0200

    Facturation ok

--- back/src/Controller/AdresseFacturationController.php
+++ back/src/Controller/AdresseFacturationController.php
@@ -13,92 +13,87 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Utilisateur;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-
 
 class AdresseFacturationController extends AbstractController
 {
- 
     #[Route('/api/adresses_facturation', name: 'getAdressesFacturation', methods: ['GET'])]
     public function getAdressesFacturation(AdresseFacturationRepository $adresseFacturationRepository, SerializerInterface $serializer): JsonResponse
     {
-        $adressesFacturation = $adresseFacturationRepository->findAll();
-        $jsonAdressesFacturation = $serializer->serialize($adressesFacturation, 'json', [
-            AbstractNormalizer::GROUPS => ['exclude']
-        ]);
-        return new JsonResponse($jsonAdressesFacturation, Response::HTTP_OK, [], true);
+        $query = $adresseFacturationRepository->createQueryBuilder('a')
+            ->select('a.id_adresse_facturation', 'a.rue', 'a.complement_adresse', 'a.region', 'a.ville', 'a.code_postal', 'a.pays');
+
+        $adressesFacturation = $query->getQuery()->getResult();
+
+        $json = $serializer->serialize($adressesFacturation, 'json');
+
+        return new JsonResponse($json, JsonResponse::HTTP_OK, ['Content-Type' => 'application/json'], true);
     }
 
     #[Route('/api/adresses_facturation/{id}', name: 'getAdresseFacturationById', methods: ['GET'])]
-    public function getAdresseFacturationById(AdresseFacturation $adresseFacturation, SerializerInterface $serializer): JsonResponse
+    public function getAdresseFacturationById(int $id, AdresseFacturationRepository $adresseFacturationRepository, SerializerInterface $serializer): JsonResponse
     {
-        $jsonAdresseFacturation = $serializer->serialize($adresseFacturation, 'json', [
-            AbstractNormalizer::GROUPS => ['exclude']
-        ]);
-        return new JsonResponse($jsonAdresseFacturation, Response::HTTP_OK, ['accept' => 'json'], true);
+        $queryBuilder = $adresseFacturationRepository->createQueryBuilder('a')
+            ->select('a.id_adresse_facturation', 'a.rue', 'a.complement_adresse', 'a.region', 'a.ville', 'a.code_postal', 'a.pays')
+            ->andWhere('a.id_adresse_facturation = :id')
+            ->setParameter('id', $id)
+            ->getQuery();
+
+        $adresseFacturation = $queryBuilder->getOneOrNullResult();
+        $jsonAdresseFacturation = $serializer->serialize($adresseFacturation, 'json');
+    
+        return new JsonResponse($jsonAdresseFacturation, JsonResponse::HTTP_OK, ['Content-Type' => 'application/json'], true);
     }
 
+    #[Route('/api/adresses_facturation', name: 'createAdresseFacturation', methods: ['POST'])]
+    public function createAdresseFacturation(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
 
-    
+        $adresseFacturation = new AdresseFacturation();
+        $adresseFacturation->setRue($data['rue']);
+        $adresseFacturation->setComplementAdresse($data['complement_adresse']);
+        $adresseFacturation->setRegion($data['region']);
+        $adresseFacturation->setVille($data['ville']);
+        $adresseFacturation->setCodePostal($data['code_postal']);
+        $adresseFacturation->setPays($data['pays']);
 
+        $entityManager->persist($adresseFacturation);
+        $entityManager->flush();
 
+        $assoAdresseUtilisateur = new AssoAdresseFacturationUtilisateur();
+        $utilisateur = $entityManager->getRepository(Utilisateur::class)->find($data['id_utilisateur']);
+        $assoAdresseUtilisateur->setUtilisateur($utilisateur);
+        $assoAdresseUtilisateur->setAdresseFacturation($adresseFacturation);
 
-#[Route('/api/adresses_facturation', name: 'createAdresseFacturation', methods: ['POST'])]
-public function createAdresseFacturation(Request $request, EntityManagerInterface $entityManager): JsonResponse
-{
-    $data = json_decode($request->getContent(), true);
+        $entityManager->persist($assoAdresseUtilisateur);
+        $entityManager->flush();
 
-    $adresseFacturation = new AdresseFacturation();
-    $adresseFacturation->setRue($data['rue']);
-    $adresseFacturation->setComplementAdresse($data['complement_adresse']);
-    $adresseFacturation->setRegion($data['region']);
-    $adresseFacturation->setVille($data['ville']);
-    $adresseFacturation->setCodePostal($data['code_postal']);
-    $adresseFacturation->setPays($data['pays']);
+        return new JsonResponse(['message' => 'Adresse de facturation ajoutée avec succès'], Response::HTTP_CREATED);
+    }
 
-    $entityManager->persist($adresseFacturation);
-    $entityManager->flush();
+    #[Route('/api/adresses_facturation/{id}', name: 'updateAdresseFacturation', methods: ['PUT'])]
+    public function updateAdresseFacturation(Request $request, EntityManagerInterface $entityManager, AdresseFacturation $adresseFacturation): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
 
-    $assoAdresseUtilisateur = new AssoAdresseFacturationUtilisateur();
-    $utilisateur = $entityManager->getRepository(Utilisateur::class)->find($data['id_utilisateur']);
-    $assoAdresseUtilisateur->setUtilisateur($utilisateur);
-    $assoAdresseUtilisateur->setAdresseFacturation($adresseFacturation);
+        $adresseFacturation->setRue($data['rue']);
+        $adresseFacturation->setComplementAdresse($data['complement_adresse']);
+        $adresseFacturation->setRegion($data['region']);
+        $adresseFacturation->setVille($data['ville']);
+        $adresseFacturation->setCodePostal($data['code_postal']);
+        $adresseFacturation->setPays($data['pays']);
 
-    $entityManager->persist($assoAdresseUtilisateur);
-    $entityManager->flush();
+        $entityManager->flush();
 
-    return new JsonResponse(['message' => 'Adresse de facturation ajoutée avec succès'], Response::HTTP_CREATED);
-}
+        return new JsonResponse(['message' => 'Adresse de facturation mise à jour avec succès'], Response::HTTP_OK);
+    }
 
+    #[Route('/api/adresses_facturation/{id}', name: 'deleteAdresseFacturation', methods: ['DELETE'])]
+    public function deleteAdresseFacturation(EntityManagerInterface $entityManager, AdresseFacturation $adresseFacturation): JsonResponse
+    {
+        $entityManager->remove($adresseFacturation);
+        $entityManager->flush();
 
-#[Route('/api/adresses_facturation/{id}', name: 'updateAdresseFacturation', methods: ['PUT'])]
-public function updateAdresseFacturation(Request $request, EntityManagerInterface $entityManager, AdresseFacturation $adresseFacturation): JsonResponse
-{
-    $data = json_decode($request->getContent(), true);
-
-    $adresseFacturation->setRue($data['rue']);
-    $adresseFacturation->setComplementAdresse($data['complement_adresse']);
-    $adresseFacturation->setRegion($data['region']);
-    $adresseFacturation->setVille($data['ville']);
-    $adresseFacturation->setCodePostal($data['code_postal']);
-    $adresseFacturation->setPays($data['pays']);
-
-    $entityManager->flush();
-
-    return new JsonResponse(['message' => 'Adresse de facturation mise à jour avec succès'], Response::HTTP_OK);
-}
-
-
-#[Route('/api/adresses_facturation/{id}', name: 'deleteAdresseFacturation', methods: ['DELETE'])]
-public function deleteAdresseFacturation(EntityManagerInterface $entityManager, AdresseFacturation $adresseFacturation): JsonResponse
-{
-    $entityManager->remove($adresseFacturation);
-    $entityManager->flush();
-
-    return new JsonResponse(['message' => 'Adresse de facturation supprimée avec succès'], Response::HTTP_OK);
-}
-
-
-
-    
+        return new JsonResponse(['message' => 'Adresse de facturation supprimée avec succès'], Response::HTTP_OK);
+    }
 }
