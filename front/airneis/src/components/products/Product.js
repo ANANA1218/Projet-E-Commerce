dcc180c0f871e0d9302d4d5b2ad30f6da6e596b5
Annabelle <annabelle.bosse.pro@gmail.com>
2023-06-14 23:57:42 +0200

    product

--- front/airneis/src/components/products/Product.js
+++ front/airneis/src/components/products/Product.js
@@ -26,35 +26,43 @@ function Product(props) {
   }
 
   return (
-    <div className="col">
+    <>
       {products.length === 0 ? (
         <div>No products found.</div>
       ) : (
         products.map((product) => (
-          <div className="card shadow-sm" key={product.id_produit}>
-            <Link to={`/products/${product.id_produit}`} href="!#" replace>
-              <img
-                className="card-img-top bg-dark cover"
-                height="200"
-                alt=""
-                src={product.image} 
-              />
-            </Link>
-            <div className="card-body">
-              <h5 className="card-title text-center text-dark text-truncate">
-                {product.nom_produit}
-              </h5>
-              <p className="card-text text-center text-muted mb-0">{product.prix}Ks</p>
-              <div className="d-grid d-block">
-                <button className="btn btn-outline-dark mt-3">
-                  <FontAwesomeIcon icon={["fas", "cart-plus"]} /> Add to cart
-                </button>
+          <div className="col" key={product.id_produit}>
+            <div className="card shadow-sm">
+              <Link to={`/products/${product.id_produit}`} href="!#" replace>
+                <img
+                  className="card-img-top bg-dark cover"
+                  height="200"
+                  alt=""
+                  src={product.image}
+                />
+              </Link>
+              <div className="card-body">
+                <h5 className="card-title text-center text-dark text-truncate">
+                  {product.nom_produit}
+                </h5>
+                <p className="card-text text-center text-muted mb-0">{product.prix}Ks</p>
+                <div className="d-grid d-block">
+                  <button className="btn btn-outline-dark mt-3">
+                    <FontAwesomeIcon icon={["fas", "cart-plus"]} /> Add to cart
+                  </button>
+                  <Link
+                    to={`/products/${product.id_produit}`}
+                    className="btn btn-outline-primary mt-2"
+                  >
+                    View More
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         ))
       )}
-    </div>
+    </>
   );
 }
 
