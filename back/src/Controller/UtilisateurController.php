0f512be7ce50bd6aef8d3b66bfde312ca608068d
Annabelle <annabelle.bosse.pro@gmail.com>
2023-06-14 20:25:59 +0200

    ok

--- back/src/Controller/UtilisateurController.php
+++ back/src/Controller/UtilisateurController.php
@@ -33,7 +33,27 @@ class UtilisateurController extends AbstractController
     }
 
 
-   
+    #[Route('/api/register', name: 'register', methods: ['POST'])]
+    public function register(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
 
+        $utilisateur = new Utilisateur();
+        $utilisateur->setNom($data['nom']);
+        $utilisateur->setPrenom($data['prenom']);
+        $utilisateur->setEmail($data['email']);
+        $utilisateur->setMotDePasse($data['mot_de_passe']);
+        $utilisateur->setTelephone($data['telephone']);
+        $utilisateur->setIdRole($data['id_role']);
+
+        $entityManager->persist($utilisateur);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Utilisateur enregistré avec succès!'], Response::HTTP_CREATED);
+    }
+
+
+
+    
 
 }
