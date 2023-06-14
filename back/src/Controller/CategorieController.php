55cbd9cb9b04196e66a7431d3b7f286a5b16d47a
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-06-15 00:01:39 +0200

    fix get all categories

--- back/src/Controller/CategorieController.php
+++ back/src/Controller/CategorieController.php
@@ -14,14 +14,14 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class CategorieController extends AbstractController
 {
- 
-    #[Route('/api/categories', name: 'index', methods: ['GET'])]
-public function index(CategorieRepository $categorieRepository, SerializerInterface $serializer): JsonResponse
-{
-    $categories = $categorieRepository->findAll();
-    $jsonCategories = $serializer->serialize($categories, 'json', ['groups' => 'category']);
-    return new JsonResponse($jsonCategories, Response::HTTP_OK, [], true);
-}
+
+    #[Route('/api/categories', name: 'getAllCategories', methods: ['GET'])]
+    public function getAllCategories(CategorieRepository $categorieRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $categories = $categorieRepository->findAll();
+        $jsonCategories = $serializer->serialize($categories, 'json', ['groups' => 'category']);
+        return new JsonResponse($jsonCategories, Response::HTTP_OK, [], true);
+    }
 
 
     #[Route('/api/categories/{id}', name: 'getOneCategorie', methods: ['GET'])]
@@ -36,44 +36,38 @@ public function index(CategorieRepository $categorieRepository, SerializerInterf
     #[Route('/api/categories', name: 'createCategorie', methods: ['POST'])]
     public function createCategorie(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        
+
         $data = json_decode($request->getContent(), true);
 
         $categorie = new Categorie();
         $categorie->setNomCategorie($data['nom_categorie']);
-      
+
         $entityManager->persist($categorie);
         $entityManager->flush();
 
-       
+
         return new JsonResponse(['message' => 'Catégorie ajoute avec succes'], Response::HTTP_CREATED);
     }
 
     #[Route('/api/categories/{id}', name: 'updateCategorie', methods: ['PUT'])]
-public function updateCategorie(Request $request, EntityManagerInterface $entityManager, Categorie $categorie): JsonResponse
-{
-    $data = json_decode($request->getContent(), true);
+    public function updateCategorie(Request $request, EntityManagerInterface $entityManager, Categorie $categorie): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
 
-    $categorie->setNomCategorie($data['nom_categorie']);
+        $categorie->setNomCategorie($data['nom_categorie']);
 
-    $entityManager->flush();
+        $entityManager->flush();
 
-    return new JsonResponse(['message' => 'Catégorie mise à jour avec succès'], Response::HTTP_OK);
-}
-
-
-#[Route('/api/categories/{id}', name: 'deleteCategorie', methods: ['DELETE'])]
-public function deleteCategorie(EntityManagerInterface $entityManager, Categorie $categorie): JsonResponse
-{
-    $entityManager->remove($categorie);
-    $entityManager->flush();
-
-    return new JsonResponse(['message' => 'Catégorie supprimée avec succès'], Response::HTTP_OK);
-}
+        return new JsonResponse(['message' => 'Catégorie mise à jour avec succès'], Response::HTTP_OK);
+    }
 
 
+    #[Route('/api/categories/{id}', name: 'deleteCategorie', methods: ['DELETE'])]
+    public function deleteCategorie(EntityManagerInterface $entityManager, Categorie $categorie): JsonResponse
+    {
+        $entityManager->remove($categorie);
+        $entityManager->flush();
 
-
-
-    
+        return new JsonResponse(['message' => 'Catégorie supprimée avec succès'], Response::HTTP_OK);
+    }
 }
