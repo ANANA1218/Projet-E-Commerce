e5e4aded185f208ddf6aeeaedc086fe08fbc6621
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-06-14 18:17:27 +0200

    delete position fixed

--- front/airneis/src/components/layouts/Footer.js
+++ front/airneis/src/components/layouts/Footer.js
@@ -1,11 +1,11 @@
 function Footer() {
-  return (
-    <footer className="mt-auto py-5 bg-dark" style={{ position: "fixed", bottom: 0, width: "100%" }}>
-      <div className="container d-flex justify-content-center">
-        <span className="text-muted">Copyright &copy; Website 2023 </span>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="mt-auto py-5 bg-dark" style={{ bottom: 0, width: "100%" }}>
+            <div className="container d-flex justify-content-center">
+                <span className="text-muted">Copyright &copy; Website 2023 </span>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
