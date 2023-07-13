95b4d4a354ca480c7b74a421196e509296cf1436
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-07-13 22:40:58 +0200

    update

--- front/airneis/src/pages/backoffice/Index.js
+++ front/airneis/src/pages/backoffice/Index.js
@@ -1,16 +1,8 @@
-import $ from 'jquery'
 import "../../assets/css/sb-admin-2.css"
 
-import Chart from "../../components/backoffice/Chart";
-import ColorSytem from "../../components/backoffice/ColorSystem";
-import Heading from "../../components/backoffice/Heading";
-import Illustration from "../../components/backoffice/Illustration";
-import PieChart from "../../components/backoffice/PieChart";
-import ProjectCard from "../../components/backoffice/ProjectCard";
-import Sidebar from "../../components/backoffice/Sidebar";
-import SmallCard from "../../components/backoffice/SmallCard";
-import Topbar from "../../components/backoffice/Topbar";
 import Header from "../../components/layouts/backoffice/Header";
+import Topbar from "../../components/backoffice/Topbar";
+import Sidebar from "../../components/backoffice/Sidebar";
 import Tableau from "../../components/backoffice/Tableau";
 
 function Index() {
@@ -21,36 +13,8 @@ function Index() {
                 <Sidebar />
                 <div id="content-wrapper" className="d-flex flex-column">
                     <div id="content">
+                        <Topbar />
                         <div className="container-fluid">
-                            <Topbar />
-                            {/* 
-                                <Heading />
-                                <div className="row">
-                                    <SmallCard />
-                                </div>
-                                <div className="row">
-                                    <Chart />
-                                    <PieChart />
-                                </div>
-                                <div className="row">
-                                    <ProjectCard />
-                                </div>
-                                <div className="row">
-                                    <div className="col-lg-6 mb-4">
-                                        <ColorSytem color="primary" />
-                                        <ColorSytem color="success" />
-                                        <ColorSytem color="info" />
-                                        <ColorSytem color="warning" />
-                                        <ColorSytem color="secondary" />
-                                        <ColorSytem color="light" />
-                                        <ColorSytem color="dark" />
-                                        <ColorSytem color="danger" />
-                                    </div>
-                                    <div className="col-lg-6 mb-4">
-                                        <Illustration />
-                                    </div>
-                                </div> 
-                            */}
                             <div className="row">
                                 <Tableau />
                             </div>
