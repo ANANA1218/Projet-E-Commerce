a7665ffd801e357042ffa1ec41fb85e822dd70dc
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-07-15 22:45:14 +0200

    modif

--- front/airneis/src/pages/backoffice/Index.js
+++ front/airneis/src/pages/backoffice/Index.js
@@ -1,28 +1,11 @@
-import "../../assets/css/sb-admin-2.css"
-
-import Header from "../../components/layouts/backoffice/Header";
-import Topbar from "../../components/backoffice/Topbar";
-import Sidebar from "../../components/backoffice/Sidebar";
-import Tableau from "../../components/backoffice/Tableau";
+import Template from "../../components/layouts/backoffice/Template";
+import Products from "./Products";
 
 function Index() {
     return (
-        <>
-            <Header />
-            <div id="wrapper">
-                <Sidebar />
-                <div id="content-wrapper" className="d-flex flex-column">
-                    <div id="content">
-                        <Topbar />
-                        <div className="container-fluid">
-                            <div className="row">
-                                <Tableau />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <Template>
+            <Products />
+        </Template>
     );
 }
 
