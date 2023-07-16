0d731e50a654f9967816dc227f55a759c6229a49
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-07-17 00:32:06 +0200

    delete

--- front/airneis/src/components/backoffice/Sidebar.js
+++ front/airneis/src/components/backoffice/Sidebar.js
@@ -63,44 +63,6 @@ function Sidebar() {
                 </div>
             </li>
 
-            <div className="sidebar-divider" style={{ borderTop: '1px solid rgba(255,255,255,.15)', margin: '10px 0' }}></div>
-
-            <div className="sidebar-heading">
-                Addons
-            </div>
-
-            <li className="nav-item">
-                <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i className="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
-                </a>
-                <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Login Screens:</h6>
-                        <a className="collapse-item" href="login.html">Login</a>
-                        <a className="collapse-item" href="register.html">Register</a>
-                        <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div className="collapse-divider"></div>
-                        <h6 className="collapse-header">Other Pages:</h6>
-                        <a className="collapse-item" href="404.html">404 Page</a>
-                        <a className="collapse-item" href="blank.html">Blank Page</a>
-                    </div>
-                </div>
-            </li>
-
-            <li className="nav-item">
-                <a className="nav-link" href="charts.html">
-                    <i className="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
-
-            <li className="nav-item">
-                <a className="nav-link" href="tables.html">
-                    <i className="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
-            </li>
-
             <div className="sidebar-divider d-none d-md-block" style={{ borderTop: '1px solid rgba(255,255,255,.15)', margin: '10px 0' }}></div>
         </ul>
     );
