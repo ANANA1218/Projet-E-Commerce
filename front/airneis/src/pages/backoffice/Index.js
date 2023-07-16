952ce6e24ef6a2d384381420e03d8e97b553131d
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-07-17 00:32:28 +0200

    histogramme

--- front/airneis/src/pages/backoffice/Index.js
+++ front/airneis/src/pages/backoffice/Index.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import BarChart from "../../components/backoffice/BarChart";
 import Template from "../../components/layouts/backoffice/Template";
-import Products from "./Products";
+
 
 function Index() {
     return (
         <Template>
+            <div className="container">
+                <div className="row justify-content-center align-items-center">
+                    <div className="col-md-10">
+                        <BarChart />
+                    </div>
+                </div>
+            </div>
         </Template>
     );
 }
