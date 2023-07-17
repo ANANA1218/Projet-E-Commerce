6a81a1f277d533ce2d94cbca7968610c6069e595
Annabelle <annabelle.bosse.pro@gmail.com>
2023-07-17 19:29:57 +0200

    ah revoir avec les adresses et autre

--- back/src/Controller/AuthController.php
+++ back/src/Controller/AuthController.php
@@ -5,35 +5,28 @@ namespace App\Controller;
 
 use App\Entity\Utilisateur;
 use App\Repository\UtilisateurRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Firebase\JWT\JWT;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
-
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface; 
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;// Ajout de l'importation manquante
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class AuthController extends AbstractController
 {
-    private EntityManagerInterface $entityManager;
     private UtilisateurRepository $utilisateurRepository;
-
     private TokenStorageInterface $tokenStorage;
-    // Inject the EntityManagerInterface through the constructor
-    public function __construct(EntityManagerInterface $entityManager, UtilisateurRepository $utilisateurRepository, TokenStorageInterface $tokenStorage)
+
+    public function __construct(UtilisateurRepository $utilisateurRepository, TokenStorageInterface $tokenStorage)
     {
-        $this->entityManager = $entityManager;
         $this->utilisateurRepository = $utilisateurRepository;
         $this->tokenStorage = $tokenStorage;
-    }
+    } 
 
-    #[Route('/api/utilisateur/login', name: 'user_login', methods: ['POST'])]
-    public function loginUser(Request $request): JsonResponse
+    #[Route('/api/utilisateur/login', name: 'login', methods: ['POST'])]
+    public function login(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
@@ -44,74 +37,23 @@ class AuthController extends AbstractController
             return $this->json(['message' => 'Email et/ou mot de passe manquant'], Response::HTTP_BAD_REQUEST);
         }
 
-        // Replace this logic with your own authentication logic
         $user = $this->utilisateurRepository->login($email, $password);
 
         if (!$user) {
             return $this->json(['message' => 'Identifiants incorrects'], Response::HTTP_UNAUTHORIZED);
         }
 
-        // Generate a token and set it on the user entity
-        $token = $this->generateToken();
-        $user->setToken($token);
-        $this->entityManager->flush();
+        // Perform any other necessary operations with the authenticated user if needed.
 
-        // Return the token in the response
-        return $this->json(['token' => $token], Response::HTTP_OK);
-    }
+        // Return only the necessary user information.
+        $userData = [
+            'id_utilisateur' => $user->getIdUtilisateur(),
+            'nom' => $user->getNom(),
+            'prenom' => $user->getPrenom(),
+            'email' => $user->getEmail(),
+            // You can add more necessary properties here if needed.
+        ];
 
-    private function generateToken(): string
-    {
-        // Generate a random token using random_bytes and convert it to hexadecimal
-        $length = 32; // Change this value according to your token length requirements
-        return bin2hex(random_bytes($length));
-    }
-
-
-    #[Route('/api/utilisateur/me', name: 'getAuthenticatedUser', methods: ['GET'])]
-    public function getAuthenticatedUser(): JsonResponse
-    {
-        $token = $this->tokenStorage->getToken();
-        if (!$token) {
-            return $this->json(['message' => 'Utilisateur non connecté'], Response::HTTP_UNAUTHORIZED);
-        }
-
-        $user = $token->getUser();
-
-        if (!$user instanceof Utilisateur) {
-            return $this->json(['message' => 'Utilisateur non valide'], Response::HTTP_UNAUTHORIZED);
-        }
-
-        // Return the authenticated user's information
-        return $this->json(['user' => [
-            'id' => $user->getIdUtilisateur(),
-            'username' => $user->getPrenom(),
-            // Add other properties you want to expose
-        ]], Response::HTTP_OK);
-    }
-
-
-    #[Route('/api/utilisateur/logout', name: 'logoutUser', methods: ['POST'])]
-    public function logoutUser(): JsonResponse
-    {
-        // Get the authenticated token
-        $token = $this->tokenStorage->getToken();
-
-        if (!$token) {
-            return $this->json(['message' => 'Utilisateur non connecté'], Response::HTTP_UNAUTHORIZED);
-        }
-
-        // Get the authenticated user
-        $user = $token->getUser();
-
-        if (!$user instanceof Utilisateur) {
-            return $this->json(['message' => 'Utilisateur non valide'], Response::HTTP_UNAUTHORIZED);
-        }
-
-        // Invalidate the user's token by setting it to null
-        $user->setToken(null);
-        $this->entityManager->flush();
-
-        return $this->json(['message' => 'Déconnexion réussie'], Response::HTTP_OK);
+        return $this->json($userData, Response::HTTP_OK);
     }
 }
