ee808c2618d635a672d0441df88b9ef3fb0787ab
Annabelle <annabelle.bosse.pro@gmail.com>
2023-07-20 18:36:56 +0200

    bon

--- front/airneis/src/pages/Home.js
+++ front/airneis/src/pages/Home.js
@@ -23,7 +23,9 @@ function Home() {
             <h2 className="text-muted text-center mt-4 mb-3">Catégories</h2>
             <div className="container pb-5 px-lg-5">
                 <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 px-md-5">
+                  
                     <ImageWithCategory/>
+                 
                 </div>
             </div>
             <div className="d-flex flex-column bg-white py-4">
