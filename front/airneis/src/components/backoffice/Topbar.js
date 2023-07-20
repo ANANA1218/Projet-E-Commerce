ce8659e89003f857dc6c034386589b771a3df107
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-07-20 23:03:16 +0200

    style

--- front/airneis/src/components/backoffice/Topbar.js
+++ front/airneis/src/components/backoffice/Topbar.js
@@ -1,30 +1,10 @@
-function Topbar() {
+function Topbar({ title }) {
 
     return (
-        <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item dropdown no-arrow d-sm-none">
-                    <a className="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i className="fas fa-search fa-fw"></i>
-                    </a>
-                    <div className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                        aria-labelledby="searchDropdown">
-                        <form className="form-inline mr-auto w-100 navbar-search">
-                            <div className="input-group">
-                                <input type="text" className="form-control bg-light border-0 small"
-                                    placeholder="Search for..." aria-label="Search"
-                                    aria-describedby="basic-addon2" />
-                                <div className="input-group-append">
-                                    <button className="btn btn-primary" type="button">
-                                        <i className="fas fa-search fa-sm"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </li>
-            </ul>
+        <nav className="navbar navbar-light bg-white topbar mb-4 static-top shadow">
+            <div className="container d-flex justify-content-center">
+                <h2 className="fw-bold text-dark">{title}</h2>
+            </div>
         </nav>
     )
 }
