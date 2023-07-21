2188c7422dbaf40bf2fc87f48c0c92eeb84ca824
Annabelle <annabelle.bosse.pro@gmail.com>
2023-07-21 09:23:12 +0200

    liste des commandes

--- front/airneis/src/pages/commande/ListCommande.js
+++ front/airneis/src/pages/commande/ListCommande.js
@@ -4,16 +4,27 @@ import { Link } from 'react-router-dom';
 
 const CommandesList = () => {
   const [commands, setCommands] = useState([]);
+  const [activeCommand, setActiveCommand] = useState(null);
+  const [commandsDetails, setCommandsDetails] = useState([]);
 
   useEffect(() => {
     const fetchCommands = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/api/commandes', {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        });
-        setCommands(response.data);
+        const [commandsResponse, commandsDetailsResponse] = await Promise.all([
+          axios.get('http://127.0.0.1:8000/api/commandes', {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+          }),
+          axios.get('http://127.0.0.1:8000/api/commande-details', {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+          }),
+        ]);
+
+        setCommands(commandsResponse.data);
+        setCommandsDetails(commandsDetailsResponse.data);
       } catch (error) {
         console.error('An error occurred while fetching commands:', error);
       }
@@ -27,17 +38,30 @@ const CommandesList = () => {
       <h2 className="mb-4">Liste des commandes</h2>
       {commands.map((commande) => (
         <div key={commande.id_commande} className="card mb-4">
-          <div className="card-body">
-            <h5 className="card-title">ID Commande: {commande.id_commande}</h5>
-            <p className="card-text">Date: {commande.date_commande}</p>
-            <p className="card-text">Prix Total: {commande.prix_total}</p>
-            <p className="card-text">Produit: {commande.nom_produit}</p>
-            <p className="card-text">Description: {commande.description}</p>
-            <p className="card-text">Prix du produit: {commande.prix}</p>
-            <Link to={`/detail-commande/${commande.id_commande}`} className="btn btn-primary">
-              Voir les détails
-            </Link>
+          <div className="card-header" onClick={() => setActiveCommand(commande.id_commande)}>
+            <h5 className="mb-0">
+              ID Commande: {commande.id_commande}
+            </h5>
           </div>
+          {activeCommand === commande.id_commande && (
+            <div className="card-body">
+              <p>Date: {commande.date_commande}</p>
+              <p>Prix Total: {commande.prix_total}</p>
+              {/* Additional details */}
+              {commandsDetails.map((commandeDetail) => {
+                if (commandeDetail.id_commande === commande.id_commande) {
+                  return (
+                    <div key={commandeDetail.id_produit}>
+                      <p>Produit: {commandeDetail.nom_produit}</p>
+                      <p>Description: {commandeDetail.description}</p>
+                      <p>Prix du produit: {commandeDetail.prix}</p>
+                    </div>
+                  );
+                }
+                return null;
+              })}
+            </div>
+          )}
         </div>
       ))}
     </div>
